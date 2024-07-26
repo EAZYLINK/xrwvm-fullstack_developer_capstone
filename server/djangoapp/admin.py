2049ad5a915d3c43e385d1d4cbdfebe6136381d7
Ezekiel Nwuguru <eazy4life2017@gmail.com>
2024-07-26 06:03:58 +0100

    feat: django models

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,11 +1,13 @@
-# from django.contrib import admin
+from django.contrib import admin
+
+from .models import CarMake, CarModel
 # from .models import related models
 
 
 # Register your models here.
-
+admin.site.register(CarMake)
 # CarModelInline class
-
+admin.site.register(CarModel)
 # CarModelAdmin class
 
 # CarMakeAdmin class with CarModelInline
